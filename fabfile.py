b0a2abce82c9fdbdb122d73bd7e4c63df7d2a02f
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2012-02-08 08:45:45 +0400

    Mostly working with master/slave setup - problem may be outside of the script, though

--- fabfile.py
+++ fabfile.py
@@ -8,19 +8,19 @@ env.shell = '/bin/bash -c'
 def build_platform(site, profile, webserver, dbserver, makefile, build):
   print "===> Building the platform..."
   run("drush make %s /var/aegir/platforms/%s" % (makefile, build))
-  run("drush --root='/var/aegir/platforms/%s' provision-save '@platform_%s' --context_type='platform'" % (build, build))
+  run("drush --root='/var/aegir/platforms/%s' provision-save '@platform_%s' --web_server='@server_%s' --context_type='platform' --debug" % (build, build, webserver))
   run("drush @hostmaster hosting-import '@platform_%s'" % build)
   run("drush @hostmaster hosting-dispatch")
 
 # Install a site on a platform, and kick off an import of the site
 def install_site(site, profile, webserver, dbserver, makefile, build):
   print "===> Installing the site for the first time..."
-  run("drush @%s provision-install" % site)
-  run("drush @hostmaster hosting-task @platform_%s verify" % build)
+  run("drush @%s provision-install --debug" % site)
+  run("drush @hostmaster hosting-task @platform_%s verify --debug" % build)
   time.sleep(5)
-  run("drush @hostmaster hosting-dispatch")
+  run("drush @hostmaster hosting-dispatch --debug")
   time.sleep(5)
-  run("drush @hostmaster hosting-task @%s verify" % site)
+  run("drush @hostmaster hosting-task @%s verify --debug" % site)
 
 # Migrate a site to a new platform
 def migrate_site(site, profile, webserver, dbserver, makefile, build):
@@ -30,7 +30,7 @@ def migrate_site(site, profile, webserver, dbserver, makefile, build):
 # Save the Drush alias to reflect the new platform
 def save_alias(site, profile, webserver, dbserver, makefile, build):
   print "===> Updating the Drush alias for this site"
-  run("drush provision-save @%s --context_type=site --uri=%s --platform=@platform_%s --server=@server_%s --db_server=@server_%s --profile=%s --client_name=admin" % (site, site, build, webserver, dbserver, profile))
+  run("drush provision-save @%s --context_type='site' --uri='%s' --platform='@platform_%s' --web_server='@server_%s' --db_server='@server_%s' --profile='%s' --debug" % (site, site, build, webserver, dbserver, profile))
 
 # Import a site into the frontend, so that Aegir learns the site is now on the new platform
 def import_site(site, profile, webserver, dbserver, makefile, build):
