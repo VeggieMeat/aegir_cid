c44a0af7a254b2f9a991d4f35f88eba259009a3e
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2012-03-15 10:11:42 +0400

    Fixed migration issue

--- fabfile.py
+++ fabfile.py
@@ -25,7 +25,7 @@ def install_site(site, profile, webserver, dbserver, makefile, build):
 # Migrate a site to a new platform
 def migrate_site(site, profile, webserver, dbserver, makefile, build):
   print "===> Migrating the site to the new platform"
-  run("drush @%s provision-migrate '@platform_%s'" % (site, build))
+  run("drush @%s provision-migrate @platform_%s --debug" % (site, build))
 
 # Save the Drush alias to reflect the new platform
 def save_alias(site, profile, webserver, dbserver, makefile, build):
@@ -35,7 +35,7 @@ def save_alias(site, profile, webserver, dbserver, makefile, build):
 # Import a site into the frontend, so that Aegir learns the site is now on the new platform
 def import_site(site, profile, webserver, dbserver, makefile, build):
   print "===> Refreshing the frontend to reflect the site under the new platform"
-  run("drush @hostmaster hosting-import @%s" % site)
-  run("drush @hostmaster hosting-task @platform_%s verify" % build)
-  run("drush @hostmaster hosting-import @%s" % site)
-  run("drush @hostmaster hosting-task @%s verify" % site)
+  run("drush @hostmaster hosting-import @%s --debug" % site)
+  run("drush @hostmaster hosting-task @platform_%s verify --debug" % build)
+  run("drush @hostmaster hosting-import @%s --debug" % site)
+  run("drush @hostmaster hosting-task @%s verify --debug" % site)
